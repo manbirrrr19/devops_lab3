d308f0dee36ba01de4f44ee6df7c10a2f0fce315
Manbir <manbirjit.togar@gmail.com>
2023-11-05 02:29:14 +0800

    change url

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,31 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+
+
+def test_bubble_sort_toomany():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 11, 23, 69, 99, 26, 21, 44]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+
+def test_bubble_sort_zero():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 0)
+
+
+def test_bubble_sort_not_int():
+    result = []
+    input_arr = ["hello", "test", "ilovedevops"]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 2)
